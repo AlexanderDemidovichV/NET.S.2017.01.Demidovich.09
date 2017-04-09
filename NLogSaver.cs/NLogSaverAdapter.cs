ff081481d2e5e1241a405ab8f4c073dc8dd2a882
PC-ADV <alex.dem.v@gmail.com>
2017-04-09 16:56:18 +0300

    logger working

---
+++ NLogSaver.cs/NLogSaverAdapter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace Task1
+{
+    public class NLogSaverAdapter : ILogger
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public void Log(LogEntry entry)
+        {
+            switch (entry.Severity)
+            {
+                case LoggingEventType.Debug: logger.Debug(entry.Exception, entry.Message);
+                    break;
+                case LoggingEventType.Error: logger.Error(entry.Exception, entry.Message);
+                    break;
+                case LoggingEventType.Information: logger.Info(entry.Exception, entry.Message);
+                    break;
+                case LoggingEventType.Fatal: logger.Fatal(entry.Exception, entry.Message);
+                    break;
+                case LoggingEventType.Warning: logger.Warn(entry.Exception, entry.Message);
+                    break;
+            }
+            
+        }
+
+        public void Flush() => LogManager.Flush();
+    }
+}
--- Task1.ConsoleTest/Program.cs
+++ Task1.ConsoleTest/Program.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using NLog;
 
 namespace Task1.ConsoleTest
 {
     class Program
     {
+
+        private static readonly ILogger logger = new NLogSaverAdapter();
+
         static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+            logger.Log(new LogEntry(LoggingEventType.Warning, "just checking"));
         }
-    }
+
+        private static void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+                logger.Log(new LogEntry(LoggingEventType.Fatal, "Unhandled exception: {0}", (Exception)e.ExceptionObject));;
+                logger.Flush();
+        }
+}
+
+
 }
